f0e82844710d63a4dc4645e982843c64669705ac
Manrul <manrul93@gmail.com>
2014-07-05 20:25:31 +0200

    Update rs232.c

--- avr/mtm/rs232.c
+++ avr/mtm/rs232.c
@@ -1,4 +1,4 @@
-     #define	F_CPU	8000000UL 
+			 #define	F_CPU	8000000UL 
 			#include <avr/io.h>				
 			#include <inttypes.h>			
 			#include <avr/interrupt.h>	
@@ -9,30 +9,32 @@
 			#include <util/delay.h>	
 
 
-
+//funkcja wysyłająca znak, otrzymany przy wywołaniu
 void USART_Transmit( unsigned char data )
 {
-
+/* Wait for empty transmit buffer */
 while ( !( UCSRA & (1<<UDRE)) )
 ;
-
+/* Put data into buffer, sends the data */
 UDR = data;
 }
 
 
 int main()
 {	
-	
+	//Ustawienie usart
 	UCSRA = 0x00;
 	UCSRB = (1<<RXEN)|(1<<TXEN);
+	//-- jesli chcemy odbierac dane to RXCIE dajemy 1 --//
+	//-- RXEN - zalaczenie odbiornika --//
 	
 	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
-
+		//ustawienie prędkości transmisji na 9600bit/s
 	UBRRH = 0;
 	UBRRL = 51;
 
 	while(1)
-	{
+	{//funkcjia wysyłająca kolejnie liczby co 100ms
 		static uint8_t i=0;
 		i++;
 
