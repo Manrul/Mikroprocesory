4ff338ea2413f03b395ece0f83ddf1f78af5db45
Manrul <manrul93@gmail.com>
2014-07-05 20:20:15 +0200

    Update timer_v1.c

--- avr/mtm/timer_v1.c
+++ avr/mtm/timer_v1.c
@@ -4,12 +4,11 @@
 		#include <avr/interrupt.h>		
 		#include <avr/pgmspace.h>		
 		#include <avr/sfr_defs.h>	
-		#include <util/delay_basic.h>	
-		#include <util/delay.h>			
+			
 
 
-volatile	uint16_t _100cntr=0; 
-
+volatile	uint16_t _100cntr=0; //zmiena typu volatile aby mogła być zmieniana wewnątrz przerwań
+//funkcje zmieniająca san portu b(zapala/gasi diody podłączone do niego)
 void LedBlink(void)
 {
 
@@ -29,25 +28,29 @@ void LedBlink(void)
 
 }
 
-
+//Funkcja obsługi przerwania
 ISR(TIMER0_OVF_vect)
 {
-	TCNT0 = 58; 
-	_100cntr++;
+	TCNT0 = 58; //zmienna licznika,tak dobrana aby przerwania występołwały 
+			// co 200us
+	_100cntr++;//zmienna inkrmentująca się wraz z każdym przerwaniem
 }
 
 
 int main(void)
 {
 
-	DDRB = 0xFF; 
-	TCCR0 = (1<<CS01); 
-	TIMSK = TIMSK | (1<<TOIE0); 
-	sei(); 
+	DDRB = 0xFF; //---PORTB na wyjscie-----//
+		//ustawienia timera
+	TCCR0 = (1<<CS01); //--- ustawiamy prescaler na 8
+	TIMSK = TIMSK | (1<<TOIE0); // | = OR ---- nie mozemy ruszac bitow
+				// od 7 do 2, zostawiamy OCIE0 (domyslnie 0),
+				// dajemy 1 na TOIE0
+	sei(); //odblokowanie przerwan
 	
 	
 	while(1)
-	{
+	{//uruchamienie funkcji co 500 przerwan
 		if(_100cntr >= 500) 
 		{
 			_100cntr = 0;	
