8ff5d1eb7db367f6093b4c10a8c88dfec77f0ea3
Manrul <manrul93@gmail.com>
2014-08-10 18:15:19 +0200

    Update Klawiatura_membranowa.c

--- avr/JezykC_T.Francuz/Klawiatura_membranowa.c
+++ avr/JezykC_T.Francuz/Klawiatura_membranowa.c
@@ -7,20 +7,20 @@
 			uint8_t SprawdzPort()
 			{
 				uint8_t y=0;
-				uint8_t x=0;
-				while(y<4)
+				uint8_t x;
+				while(y<4) //Ponieważ są 4 rzędy klawiszy
 				{
 					x=PORTA;
-					x=(x|0x0F)^(1<<y);
+					x=(x|0x0F)^(1<<y);// Sprawdz który rząd jest aktywny
 					PORTA=x;
 					asm volatile ("nop");
 					x=PINA;
-					if((x & 0x70)!=0x70)
+					if((x & 0x70)!=0x70)//sprawdza czy jakis klawisz jest wcisniety
 					{
-						if(( x & 0x10)==0) x=0;
+						if(( x & 0x10)==0) x=0;//sprawdza z której kolumny jest ten klawisz
 						else if((x & 0x20)==0) x=1;
 						else x=2;
-						x=x+y*3;
+						x=x+y*3;//wyznacza numer wybranego klawisza
 						return x;
 					}
 					y++;
@@ -32,12 +32,8 @@
 
 			void main()
 			{
-				DDRA=0x0F;
-				PORTA=0x7F;
-				DDRB = 0xFF;
-				PORTA=0x00;
-				
-				
+				DDRA=0x0F; //4 rzedy to wyjscia,3 kolumny to wejscia
+				PORTA=0x7F;//włacza pull up na kolumnach,ustawia 1 w rzedach
 				
 				while(1);
 				
