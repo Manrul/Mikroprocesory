5ad9e1bfdd1ec1a6bb166e7716b986f8435639f0
Manrul <manrul93@gmail.com>
2014-08-16 21:36:44 +0200

    Pamięc flash

--- avr/moduly/flash.c
+++ avr/moduly/flash.c
@@ -1,22 +1,25 @@
-#include <avr/pgmspace.h>//biblioteka z potrzebnymi definicjami i funkcjami
+//biblioteka z potrzebnymi definicjami i funkcjami
+
+#include <avr/pgmspace.h>
 
 
 const char __attribute__((__progmem__))  x[]="xxx";//przykład zapisu łańcucha znaków do pamięci flash
 const char PROGMEM x1[]="xxx";
-//PRGMEM jest makrem zastępującym zapis __attribute__((__progmem__))
+//PROGMEM jest makrem zastępującym zapis __attribute__((__progmem__))
 
 
 
-//Funkcje odpowiadające za ODCZYT danych za pamięci flash 
+//Funkcje odpowiadające za ODCZYT danych z pamięci flash 
 
 pgm_read_byte(adres);//zwraca bajt danych spod podanego adresu
-pgm_read_word(adres);//zwraca 2 bajty
-pgm_read_dword(adres);//zwraca 4 bakty
-pgm_read_float(adres);//zwraca wartość  o typie float 
+pgm_read_word(adres);//zwraca 2 bajty danych spod podanego adresu
+pgm_read_dword(adres);//zwraca 4 bakty danych spod podanego adresu
+pgm_read_float(adres);//zwraca dane o typie float spod podanego adresu
 
-//Zapis tablicy do pamięci flash
+
+//Zapis typu złożonego  do pamięci flash
+
 const char PROGMEM m1[]="zamknij";
 const char PROGMEM m2[]="otwórz";
 const char PROGMEM m3[]="wybierz";
-
 const char * const Menu[3] PROGMEM = {m1,m2,m3};
